233e7095ed0f8b9537e2a1c37a4bdbac6ef9d528
monkeycoderw <3897177+monkeycoderw@users.noreply.github.com>
2019-08-17 22:43:21 +0800

    defer

--- src/main/jikeGo/ch10/func/func_test.go
+++ src/main/jikeGo/ch10/func/func_test.go
@@ -7,10 +7,16 @@ import (
 	"time"
 )
 
+/**
+  多返回值
+ */
 func returnMultiValues () (int, int) {
 	return rand.Intn(10), rand.Intn(20)
 }
 
+/**
+  计时监控
+ */
 func timeSpent(inner func(op int) int) func (op int) int {
 	return func(n int) int {
 		start:= time.Now()
@@ -20,14 +26,44 @@ func timeSpent(inner func(op int) int) func (op int) int {
 	}
 }
 
+/**
+  睡眠函数
+ */
 func slowFun(op int) int {
 	time.Sleep(time.Second * 1)
 	return op
 }
 
+func sum(ops ...int) int {
+	ret := 0
+	for _,op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+/**
+
+ */
 func TestFn(t *testing.T) {
 	a,b := returnMultiValues()
 	t.Log(a,b)
 	tsSf := timeSpent(slowFun)
 	t.Log(tsSf(10))
+	t.Log(sum(1,2,3,4,5))
+	t.Log(sum(1,2,3,4,5,6))
+
 }
+
+func clear() {
+	fmt.Println("Clear resources.")
+}
+
+func TestDefer(t *testing.T) {
+ 	defer clear()
+ 	fmt.Println("Start")
+ 	panic("err")
+}
+
+
+
